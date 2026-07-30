fbdecc2f94f295db3c2c2f135aed7b1bbd7197e8
EmmaCodez <emmacodez.dev@gmail.com>
2026-07-30 20:41:29 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+// import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import GsapFrom from "./pages/GsapFrom";
+import GsapFromTo from "./pages/GsapFromTo";
+import GsapScrollTrigger from "./pages/GsapScrollTrigger";
+import GsapStagger from "./pages/GsapStagger";
+import GsapText from "./pages/GsapText";
+import GsapTimelime from "./pages/GsapTimelime";
+import GsapTo from "./pages/GsapTo";
+
+const App = () => {
+  return (
+    <main>
+      {/* <Home /> */}
+      {/* <GsapFrom />
+      <GsapFromTo />
+      <GsapScrollTrigger />
+      <GsapStagger />
+      <GsapText />
+      <GsapTimelime />
+      <GsapTo /> */}
+
+      <Navbar/>
+    </main>
+  );
+};
+
+export default App;
