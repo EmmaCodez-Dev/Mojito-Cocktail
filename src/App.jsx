412d2a4beb5811804637ee70229a2d5487f021b2
EmmaCodez <emmacodez.dev@gmail.com>
2026-07-31 20:38:12 +0100

    implemented the ART section

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
@@ -65,6 +66,7 @@ const App = () => {
         <Hero />
         <Cocktails />
         <About />
+        <Art/>
       </div>
     </main>
   );
